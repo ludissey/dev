94508a1ce609467288309b67900ed29845f7e08d
ludissey <65865569+ludissey@users.noreply.github.com>
2021-12-31 18:27:08 +0100

    patch 1

--- main.cpp
+++ main.cpp
@@ -11,14 +11,24 @@ using namespace std;
 int main() {
     ofstream fout ("milk2.out");
     ifstream fin ("milk2.in");
-    int N;
+    int N, l, p;
+    l=p=0;
     vector<pair<int, int>> CT;
     fin >> N;
+
+    //array de toute les temps possible
     for(int i=0; i<N; i++){
         int s, e;
         fin >> s >> e;
         if(CT.size == 0) CT[0].first==s; CT[0].second==e;
+        else{
+            if(CT[CT.size-1].second >= e+1 && CT[CT.size-1].first >= s) CT[CT.size-1].second = e;
+            else CT.pop(make_pair(s, e)); p = s - CT[CT.size-1].second;
+        }
     }
-    fout << a+b << endl;
+    for(int i=0; i<CT.size; i++){
+        l = max(l, CT.second - CT.first);
+    }
+    fout << l << " " << p << endl;
     return 0;
 }
