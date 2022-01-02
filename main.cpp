e3ecf377ca4c75e0159d3c512a98c35ac789f28b
ludissey <65865569+ludissey@users.noreply.github.com>
2022-01-02 16:32:40 +0100

    implemented own version

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 /*
 ID: unityjo1
 TASK: milk2
@@ -7,8 +8,7 @@ LANG: C++
 #include <algorithm>
 #include <fstream>
 #include <vector>
-#include <iostream>
-#include <vector>
+
 
 using namespace std;
 
@@ -21,29 +21,26 @@ int main() {
     fin >> N;
 
     //array de toute les temps possible
-    for(int i=0; i<=N; i++){
+    for(int i=0; i<N; i++){
         int s, e;
         fin >> s >> e;
         CT.push_back(make_pair(s, e));
     }
-
     sort(CT.begin(), CT.end());
-    int head, _head;
-    int tail, _tail;
-    head = _head = CT[0].first;
-    tail = _tail = CT[0].second;
+    int head, tail;
+    head = CT[0].first;
+    tail = CT[0].second;
 
-    for(int j=1; j<CT.size(); j++){
-        //temps le plus long 
-        if(CT[j].first <= tail && CT[j].second > tail) tail = CT[j].second; 
-        else head = CT[j].first; tail = CT[j].second;
-        l = max(l, tail-head);
-
-        //temps le plus long de pause
-        if(CT[j].first >= _tail) p = max(p, CT[j].first - _tail); _head = CT[j].first;
-        _tail = CT[j].second;
+    for(int i=1; i<CT.size(); i++){
+        if(CT[i].first<=tail && CT[i].second>tail){
+            tail = CT[i].second;
+        }else{
+            p=max(p, CT[i].first-tail);
+            l=max(l, tail-head);
+            head = CT[i].first;
+            tail = CT[i].second;
+        }
     }
-
     fout << l << " " << p << endl;
     return 0;
 }
