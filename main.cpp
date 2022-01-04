1b21b6c30880a4bee35914b95650c86c9d2f837f
ludissey <65865569+ludissey@users.noreply.github.com>
2022-01-04 12:57:41 +0100

    transpose

--- main.cpp
+++ main.cpp
@@ -11,10 +11,28 @@ LANG: C++
 using namespace std;
 
 int main() {
+    vector<vector<char>> grid;
     ofstream fout ("transform.out");
     ifstream fin ("transform.in");
-    int a, b;
-    fin >> a >> b;
+    int N;
+    fin >> N;
+    for(int i=0; i<N; i++){
+        vector<char> rows;
+        for(int j=0; j<N; j++){
+            char in;
+            fin >> in;
+            rows.push_back(in);
+        }
+        grid.push_back(row);
+    }
     fout << a+b << endl;
     return 0;
 }
+void transpose(vector<vector<char>> grid){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            grid[i][j] = grid[j][i];
+        }
+    }
+    return grid;
+} 
