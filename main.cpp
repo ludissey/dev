b8a707d8b02bcffd71da0d2ec6b3785a35a8cce2
ludissey <65865569+ludissey@users.noreply.github.com>
2022-01-06 14:09:41 +0100

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,40 @@
 /*
 ID: unityjo1
-TASK: transform
+TASK: palsquare
 LANG: C++                 
 */
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-int N;
-
-bool rotate90(vector<vector<char>> grid, expect){
-    vector<vector<char>> copy = grid;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            copy[i][j] = grid[j][i];
-            copy[i][j] = copy[abs(N-j-1)][y];
-            if(copy[i][j] != expect[i][j]) return false;
-        }
-    }
-    return true;
-} 
+void reverse(string& str)
+{
+    int n = str.length(); 
+    for (int i = 0; i < n / 2; i++) swap(str[i], str[n - i - 1]);
+}
+bool isPalindrome(int a){
+    
+    string pal = to_string(a);
+    reverse(pal);
+    if(pal == to_string(a)) return true;
+    return false;
+}
+ 
+int B;
 
 int main() {
-    vector<vector<char>> grid, expc;
-    ofstream fout ("transform.out");
-    ifstream fin ("transform.in");
-    fin >> N;
-    for(int i=0; i<N; i++){
-        vector<char> rows;
-        for(int j=0; j<N; j++){
-            char in;
-            fin >> in;
-            rows.push_back(in);
-        }
-        grid.push_back(rows);
-    }
+    ofstream fout ("palsquare.out");
+    ifstream fin ("palsquare.in");
     
-    for(int i=0; i<N; i++){
-        vector<char> rows;
-        for(int j=0; j<N; j++){
-            char in;
-            fin >> in;
-            expc.push_back(in);
-        }
-        expc.push_back(rows);
-    }
-    if(rotate90(grid, expc)){ fout << '1' << endl;}
+    fin >> B;
+    
+    for(int i=1; i<=300; i++){
+        if(int(reverse(to_string(i))[0])==B){}
+        if(isPalindrome(i*i)){ fout << i << " " << i*i << "\n";}
+    } 
     return 0;
 }
-
